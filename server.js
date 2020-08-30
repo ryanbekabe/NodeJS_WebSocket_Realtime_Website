24a314d9e4a137d3ccceeb7472be104305a1beb5
ryanbekabe <bekabeipa@gmail.com>
2020-08-30 20:05:29 +0700

    NodeJS WebSocket Realtime Website Stream Data

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 console.log("Server JS NodeJS https://www.youtube.com/watch?v=cXQGJ_8QYK0")
 var express = require("express")
 var app = express();
+app.use(function (request, result, next) {
+	result.setHeader("Access-Control-Allow-Origin", "*");
+	next();
+});
+
 var http = require("http").createServer(app);
 
 var io = require("socket.io")(http);
@@ -25,8 +30,14 @@ io.on("connection", function (socket) {
 	});
 });
 
+app.get("/get_messages", function (request, result) {
+	connection.query("SELECT * FROM messages", function (error, messages) {
+		result.end(JSON.stringify(messages));
+	});
+});
+
 app.get("/", function (request, result) {
-	result.send("Bismillah...");
+	result.send("Bismillah... Hello world !");
 });
 
 var port = 3000;
